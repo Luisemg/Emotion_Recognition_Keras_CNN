8d49ea6be1281037af4f2d1254656345614b27ed
Luisemg <59816420+Luisemg@users.noreply.github.com>
2020-05-28 10:37:52 -0600

    Add files via upload

--- NHS_Face_Recognition.py
+++ NHS_Face_Recognition.py
@@ -1,3 +1,10 @@
+from keras.models import load_model
+from time import sleep
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing import image
+import cv2
+import numpy as np
+
 face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 classifier = load_model('NHS_vgg.h5')
 
